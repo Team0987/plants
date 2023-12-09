0eff041a03a51d16b62dec19a7090fe3ce94ad8e
unknown <153218810+Team0987@users.noreply.github.com>
2023-12-09 21:06:22 +0630

    CRUD operation is compete for blog-posts

--- api/app.js
+++ api/app.js
@@ -21,6 +21,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static( '/public'));
 app.set('view engine', 'jade');
 
 app.use(fileupload())
--- api/routes/blog.js
+++ api/routes/blog.js
@@ -32,10 +32,39 @@ router.put('/getPost', async(req,res,next)=>{
    await postModel.findOne({_id:id})
    .then(result=>{res.json(result)
     console.log("post is found")
-})
-.catch(e=>{console.log("error is occur",e)
+    })
+    .catch(e=>{console.log("error is occur",e)
     res.json("error",e)
+});
+
+router.post('/update',async(req,res,next)=>{
+  console.log('update route is called')
+    const {files,body}=req;
+    const fileName = files.img.name;
+    const filePath = 'public/images/' + fileName;
+    files.img.mv(filePath,(err)=>{
+    console.log(err);
+    });
+    let id = body.postid   
+    //db.students.updateOne( { _id: 3 }, [ { $set: { "test3": 98, modified: "$$NOW"} } ] )
+    await postModel.updateOne({ _id:id },[
+      { $set :{
+        "title":body.title,
+        "summery":body.summery,
+        "detailPost":body.detail,
+        "tags":body.tags,
+        "image":fileName
+         }
+        }
+    ])
+   .then(res.json("Article Created"))
 })
-; 
+
+router.put('/removePost',async(req,res,next)=>{
+    let id = req.body.id;
+    await postModel.findOneAndDelete({_id:id})
+    .then(res.json("success"))
+    
+})
 })
 module.exports = router;
